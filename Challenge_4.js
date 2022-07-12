eb283e3fc0c1781bcffd29887dc6820793608747
N0mekop <blinkevin@hotmail.com>
2022-07-12 16:07:03 +0200

    first match values

---
+++ Challenge_4.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const teamDauphins = "Dauphins";
+const teamKoalas = "Koalas";
+
+let firstMatchDauphinsScores = {
+    score1 : 96,
+    score2 : 108,
+    score3 : 89,
+}
+
+let firstMatchKoalasScores = {
+    score1 : 88,
+    score2 : 91,
+    score3 : 110,
+}
+
+let secondMatchDauphinsScores = {
+    score1 : 97,
+    score2 : 112,
+    score3 : 101,
+}
+
+let secondMatchKoalasScores = {
+    score1 : 109,
+    score2 : 95,
+    score3 : 123,
+}
+
+let thirdMatchDauphinsScores = {
+    score1 : 97,
+    score2 : 112,
+    score3 : 101,
+}
+
+let thirdMatchKoalasScores = {
+    score1 : 109,
+    score2 : 95,
+    score3 : 106,
+}
+
+console.log(
+    `\n`,
+    `Dauphins score 1: ${firstMatchDauphinsScores.score1}\n`,
+    `Dauphins score 2: ${firstMatchDauphinsScores.score2}\n`,
+    `Dauphins score 3: ${firstMatchDauphinsScores.score3}\n`,
+    `Koalas score 1: ${firstMatchKoalasScores.score1}\n`,
+    `Koalas score 2: ${firstMatchKoalasScores.score2}\n`,
+    `Koalas score 3: ${firstMatchKoalasScores.score3}\n`,
+    );
