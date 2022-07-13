50dc92f71926a175b0318ad7feb0f6014c371a7b
N0mekop <blinkevin@hotmail.com>
2022-07-13 15:10:21 +0200

    challenge complete?

--- Challenge_4.js
+++ Challenge_4.js
@@ -39,7 +39,7 @@ let thirdMatchKoalasScores = {
     score3 : 106,
 }
 
-console.log(
+/* console.log(
     `\n`,
     `Dauphins score 1: ${firstMatchDauphinsScores.score1}\n`,
     `Dauphins score 2: ${firstMatchDauphinsScores.score2}\n`,
@@ -67,5 +67,74 @@ console.log(
     `Koalas score 1: ${thirdMatchKoalasScores.score1}\n`,
     `Koalas score 2: ${thirdMatchKoalasScores.score2}\n`,
     `Koalas score 3: ${thirdMatchKoalasScores.score3}\n`,
-    );
+    ); */
 
+function calculAverageScore(score1, score2, score3) {
+    return (score1 + score2 + score3) / 3;
+} 
+
+/* TEAM DAUPHINS */
+
+const firstMatchDauphinsAverageScores = calculAverageScore(
+    firstMatchDauphinsScores.score1,
+    firstMatchDauphinsScores.score2,
+    firstMatchDauphinsScores.score3,
+);
+
+console.log(`La moyenne du premier match pour les ${teamDauphins} est de: ${firstMatchDauphinsAverageScores}`);
+
+const secondMatchDauphinsAverageScores = calculAverageScore(
+    secondMatchDauphinsScores.score1,
+    secondMatchDauphinsScores.score2,
+    secondMatchDauphinsScores.score3,
+);
+
+/* console.log(`La moyenne du second match pour les ${teamDauphins} est de: ${secondMatchDauphinsAverageScores}`); */
+
+const thirdMatchDauphinsAverageScores = calculAverageScore(
+    thirdMatchDauphinsScores.score1,
+    thirdMatchDauphinsScores.score2,
+    thirdMatchDauphinsScores.score3,
+);
+
+/* console.log(`La moyenne du troisième match pour les ${teamDauphins} est de: ${thirdMatchDauphinsAverageScores}`); */
+
+/* TEAM KOALAS */
+
+const firstMatchKoalasAverageScores = calculAverageScore(
+    firstMatchKoalasScores.score1,
+    firstMatchKoalasScores.score2,
+    firstMatchKoalasScores.score3,
+);
+
+console.log(`La moyenne du premier match pour les ${teamKoalas} est de: ${firstMatchKoalasAverageScores}`);
+
+const secondMatchKoalasAverageScores = calculAverageScore(
+    secondMatchKoalasScores.score1,
+    secondMatchKoalasScores.score2,
+    secondMatchKoalasScores.score3,
+);
+
+/* console.log(`La moyenne du second match pour les ${teamKoalas} est de: ${secondMatchKoalasAverageScores}`); */
+
+const thirdMatchKoalasAverageScores = calculAverageScore(
+    thirdMatchKoalasScores.score1,
+    thirdMatchKoalasScores.score2,
+    thirdMatchKoalasScores.score3,
+);
+
+/* console.log(`La moyenne du troisième match pour les ${teamKoalas} est de: ${thirdMatchKoalasAverageScores}`); */
+
+/* GAGNANT */
+
+if(firstMatchDauphinsAverageScores > 100 || firstMatchKoalasAverageScores > 100) {
+    if (firstMatchDauphinsAverageScores > firstMatchKoalasAverageScores) {
+        console.log(`Les gagnants sont les: ${teamDauphins} !!!`);
+    } else if (firstMatchDauphinsAverageScores < firstMatchKoalasAverageScores) {
+        console.log(`Les gagnants sont les: ${teamKoalas} !!!`);
+    } else {
+        console.log(`Egalité`)
+    }
+} else {
+    console.log(`Aucune équipe n'a obtenue 100 pts`)
+}
